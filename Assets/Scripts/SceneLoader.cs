84cc0bee4a7d560177b6622d2e3d899271d9c5b2
Atigerlee <atiger.lee2016@gmail.com>
2025-04-28 20:55:53 +0800

    add start scene and start game logic

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+        Debug.Log("開始遊戲");
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+        Debug.Log("退出遊戲。");
+    }
+}
